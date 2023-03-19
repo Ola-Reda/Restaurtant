df0a008b66d0a3c7eb194fd5655e6c3fc2526a96
ola <78170370+Ola-Reda@users.noreply.github.com>
2023-03-19 03:13:19 +0200

    add code

--- js/demo.js
+++ js/demo.js
@@ -1,9 +1,20 @@
 let swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
-    spaceBetween: 30,
+    spaceBetween: 25,
     pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
     },
 });
 
